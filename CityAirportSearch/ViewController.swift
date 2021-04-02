972f43deb2c6aa059de865845ad7cc12a61f9ea4
v.kovalevskiy <v.kovalevskiy.ios@gmail.com>
2021-04-02 07:57:35 +0300

    Initial Commit

---
+++ CityAirportSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityAirportSearch
+//
+//  Created by Valeriy Kovalevskiy on 2.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
